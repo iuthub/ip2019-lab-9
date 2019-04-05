2c0b74dac79c86214701316ba24920efd9a45258
desmonduz <sarrtv@gmail.com>
2019-04-05 11:38:41 +0500

    first solution

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,42 @@
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
 |
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('blog.index');
+})->name('blog.index');
+
+Route::get('post/{id}', function () {
+    return view('blog.post');
+})->name('blog.post');
+
+Route::get('about', function () {
+    return view('other.about');
+})->name('other.about');
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('', function () {
+        return view('admin.index');
+    })->name('admin.index');
+
+    Route::get('create', function () {
+        return view('admin.create');
+    })->name('admin.create');
+
+    Route::post('create', function() {
+        return "It works!";
+    })->name('admin.create');
+
+    Route::get('edit/{id}', function () {
+        return view('admin.edit');
+    })->name('admin.edit');
+
+    Route::post('edit', function() {
+        return "It works!";
+    })->name('admin.update');
 });
